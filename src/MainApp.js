c84413f1eeea5df588c4e02936e29107eb72f5a4
Lia <lia.tabilo@gmail.com>
2019-09-15 19:22:18 +0100

    Initial commit

---
+++ src/MainApp.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import './MainApp.css';
+
+class MainApp extends Component {
+	render() {
+		return (
+			<div>
+				<h1>
+					<header className="header .d-sm-flex">
+						Na'dia
+						<br />
+						Correia Gonc,alves
+					</header>
+				</h1>
+			</div>
+		);
+	}
+}
+
+export default MainApp;
